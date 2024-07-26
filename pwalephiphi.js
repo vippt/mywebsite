1abe7b39564291af2e5808d598d731ab49b60351
vippt <lephiphi.com.2017@gmail.com>
2024-07-26 12:54:49 +0700

    Update pwalephiphi.js

--- pwalephiphi.js
+++ pwalephiphi.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
+        navigator.serviceWorker.register('/mywebsite/sw.js').then(registration => {
             console.log('Service Worker registered with scope:', registration.scope);
         }).catch(error => {
             console.log('Service Worker registration failed:', error);
